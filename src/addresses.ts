3fa5ff91ad06f5df4ff2da045f57842cc623864a
kassandra <0xdada@protonmail.com>
2023-02-10 09:32:11 -0500

    fix: addresses import

--- src/addresses.ts
+++ src/addresses.ts
@@ -18,7 +18,10 @@ export interface Addresses {
 }
 
 export const getAddresses = (network: string): Addresses => {
-  const coreAddresses = getCoreAddresses(network);
+  const coreAddresses =
+    network == "local" || network == "hardhat"
+      ? getCoreAddresses("mainnet")
+      : getCoreAddresses(network);
   switch (network) {
     case "hardhat":
       return {
@@ -61,7 +64,7 @@ export const getAddresses = (network: string): Addresses => {
         ArrakisV2Factory: coreAddresses.ArrakisV2Factory,
         OneInchRouter: "",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        WETH: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", /// IMPORTANT: must be WMATIC
+        WETH: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", // IMPORTANT: must be WMATIC
         DAI: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
         USDC: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
         faucetDai: "0x27F8D03b3a2196956ED754baDc28D73be8830A6e",
@@ -99,6 +102,22 @@ export const getAddresses = (network: string): Addresses => {
         faucetDai: "",
         faucetUSDC: "",
         faucetWeth: "",
+      };
+    case "local":
+      return {
+        UniswapV3Factory: coreAddresses.UniswapV3Factory,
+        ArrakisV2: coreAddresses.ArrakisV2Implementation,
+        ArrakisV2Resolver: coreAddresses.ArrakisV2Resolver,
+        ArrakisV2Helper: coreAddresses.ArrakisV2Helper,
+        ArrakisV2Factory: coreAddresses.ArrakisV2Factory,
+        OneInchRouter: "0x1111111254fb6c44bac0bed2854e76f90643097d",
+        SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+        WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+        USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        faucetDai: "0x075e72a5edf65f0a5f44699c7654c1a76941ddc8",
+        faucetUSDC: "0x0a59649758aa4d66e25f08dd01271e891fe52199",
+        faucetWeth: "0xf04a5cc80b1e94c69b48f5ee68a08cd2f09a7c3e",
       };
     default:
       throw new Error(`No addresses for Network: ${network}`);
